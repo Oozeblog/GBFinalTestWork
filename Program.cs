faaaae5bb68268c18a49efa9f16fa7b7b654229f
ooze <grigorianna@gmail.com>
2022-11-30 19:14:50 +0400

    Added CreateThreeCharArray

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿string[] CreateRandomArray(int size)
+﻿string[] CreateArray(int size)
 {
-    string [] arrayString = new string[size]; 
+    string[] arrayString = new string[size]; 
     for (int i = 0; i < size; i++)
     {
         Console.WriteLine("Введите слово: ");
@@ -16,10 +16,33 @@ void ShowArray(string[] array)
     }
     Console.WriteLine();
 }
+string[] CreateThreeCharArray(string[] array)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        size++;
+    }
+    string[] threeCharArray = new string[size];
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < threeCharArray.Length; j++)
+        {
+            if(array[i].Length <= 3)
+            threeCharArray[j] = array[i];
+        }
+    }
+    return threeCharArray;
+}
 Console.WriteLine("Введите колличество элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-string[] myArray = CreateRandomArray(n);
+string[] myArray = CreateArray(n);
+Console.WriteLine("Исходный массив строк: ");
 ShowArray(myArray);
 
+Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам: ");
+string [] newArray = CreateThreeCharArray(myArray);
+ShowArray(newArray);
