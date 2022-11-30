11ada1b22c7587d0792af6140d54b772ab466297
ooze <grigorianna@gmail.com>
2022-11-30 18:48:00 +0400

    Added CreateArray and ShowArray

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] CreateRandomArray(int size)
+{
+    string [] arrayString = new string[size]; 
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите слово: ");
+        arrayString[i] = Console.ReadLine();
+    }
+    return arrayString;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите колличество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = CreateRandomArray(n);
+ShowArray(myArray);
+
